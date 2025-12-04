1e33d8f535495dad1eb50478ff35034542c73b58
CodeManMalck <51975207+CodeManMalck@users.noreply.github.com>
2025-12-03 18:36:50 -0800

    Add files via upload

---
+++ instructions.md
@@ -0,0 +1,1 @@
+Rewrite the for loop as a while loop.
---
+++ main.js
@@ -0,0 +1,9 @@
+// Leave this one in place
+var sum = 0;
+
+for (var i = 0; i < 5; i++) {
+    sum += i;
+    console.log("My current sum is: " + sum);
+}
+
+// Then write the while loop version below
