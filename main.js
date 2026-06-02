628802df6e9298649e57db85f714228fbce9283f
sanemondhuynh12-cmd <sanemond.huynh12@gmail.com>
2026-06-02 03:54:26 +0000

    Change for loop to while loop

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // Leave this one in place
 var sum = 0;
 
-for (var i = 0; i < 5; i++) {
-    sum += i;
-    console.log("My current sum is: " + sum);
-}
+ for (var i = 0; i < 5; i++) {
+     sum += i;
+     console.log("My current sum is: " + sum);
+ }
 
 // Then write the while loop version below
+var sum = 0;
+var i = 0;
+
+while (i<5) {
+    sum += i;
+    console.log("My current sum is: " + sum);
+    i++;
+}
